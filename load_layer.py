2f3d3129614e1e1d1e0b76fa16b479c45a11e27b
aaronjutzi <72082209+aaronjutzi@users.noreply.github.com>
2021-12-26 22:08:50 -0500

    Update load_layer.py

--- load_layer.py
+++ load_layer.py
@@ -1,5 +1,7 @@
 # This script can be used to load generic vector layers into your QGIS project
 # It will load the script with the CRS of your choosing and change the colour to black
+# Use for reference: https://docs.qgis.org/3.16/en/docs/pyqgis_developer_cookbook/loadlayer.html
+
 
 CRS = "" # Enter EPSG number in between the quotations (Example: "26917" for NAD83/ UTM Zone 17N)
 
@@ -9,9 +11,17 @@ QgsProject.instance().setCrs(QgsCoordinateReferenceSystem(CRS))
 # Complete these variables for the addVectorLayer method to work
 file_path = ""
 layer_name = ""
-data_type = "" # Example: "OGR"
+data_type = "" # Example: "ogr"
 
+# layer is added
 layer = iface.addVectorLayer(file_path, layer_name, data_type)
 
-layer.renderer().symbol().setColor(QColor("black"))
+
+# Feel free to change the layer colour if you want to
+colour = "black"
+
+# Changes layer colour
+layer.renderer().symbol().setColor(QColor("colour"))
+
+# Refreshes QGIS so colour change can be seen
 layer.triggerRepaint()
